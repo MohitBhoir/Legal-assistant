fd3bb844b8f3e6cf7e41ce227746a3987583b50c
MohitBhoir <mohitbhoir93@gmail.com>
2024-12-25 12:18:00 +0530

    extra ui

--- legal-assistant/app/forum/page.js
+++ legal-assistant/app/forum/page.js
@@ -1,12 +1,7 @@
-import Chat from '../components/Chat';
 import Navbar from '../components/navbar';
 
 export default function Forum() {
   return <>
     <Navbar/>
-    <div className="min-h-screen bg-transparent">
-      <h1 className="text-3xl text-center text-indigo-700 font-semibold my-8">Chat Forum</h1>
-      <Chat />
-    </div>
   </>
 }
