238e937e6caa8ae207e3f7f74dd5fe115947685e
MohitBhoir <mohitbhoir93@gmail.com>
2025-01-01 11:59:58 +0530

    loader change

--- legal-assistant/app/components/FindLawyer.js
+++ legal-assistant/app/components/FindLawyer.js
@@ -3,11 +3,11 @@
 import { useState, useEffect } from "react";
 import * as mammoth from "mammoth";
 import * as pdfjsLib from "pdfjs-dist/webpack";
-import Loader from "./Loader";
+// import Loader from "./Loader";
 
 export default function FindLawyer() {
   const [inputType, setInputType] = useState("text");
-  const [loader,setLoader]=useState(false)
+  // const [loader,setLoader]=useState(false)
   const [input, setInput] = useState("");
   const [selectedAction, setSelectedAction] = useState("default");
   const [results, setResults] = useState([]);
@@ -78,7 +78,7 @@ export default function FindLawyer() {
 
     if (selectedAction === "findSimilarLawyer") {
       try {
-        setLoader(true)
+        // setLoader(true)
         const response = await fetch("/api/lawyers", {
           method: "POST",
           headers: {
@@ -87,9 +87,9 @@ export default function FindLawyer() {
           body: JSON.stringify({ query: input }),
         });
 
-        if(response.ok){
-           setLoader(false)
-        }
+        // if(response.ok){
+        //    setLoader(false)
+        // }
         if (!response.ok) {
           throw new Error("Failed to fetch lawyer details");
         }
@@ -101,7 +101,7 @@ export default function FindLawyer() {
       }
     } else {
       try {
-        setLoader(true)
+        // setLoader(true)
         const response = await fetch("/api/verdict", {
           method: "POST",
           headers: {
@@ -110,9 +110,9 @@ export default function FindLawyer() {
           body: JSON.stringify({ user_input: input }),
         });
 
-        if(response.ok){
-            setLoader(false)
-        }
+        // if(response.ok){
+        //     setLoader(false)
+        // }
         if (!response.ok) {
           throw new Error("Failed to fetch verdict and similar cases");
         }
@@ -207,8 +207,7 @@ export default function FindLawyer() {
     </button>
   </form>
   {error && <p className="text-red-600 mt-4">{error}</p>}
-      {selectedAction == "findSimilarLawyer" && (
-         setLoader ? <Loader/> : <ul className="mt-6 w-full max-w-lg overflow-y-auto max-h-96">
+      {selectedAction == "findSimilarLawyer" && ( <ul className="mt-6 w-full max-w-lg overflow-y-auto max-h-96">
           {results.map((result, index) => (
             <li
               key={index}
