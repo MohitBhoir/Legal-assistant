4151921fc1721f2cb7032470319343445423b672
Preet12052003 <jainpreet12052003@gamil.com>
2025-04-01 14:21:59 +0530

    fixed some auth related bugs

--- legal-assistant/app/components/navbar.js
+++ legal-assistant/app/components/navbar.js
@@ -11,7 +11,7 @@ export default function Nav() {
   return (
     <Navbar fluid rounded className="h-auto sticky top-0 z-50 bg-slate-200 shadow-xl">
       <Navbar.Brand href="/" className="text-3xl font-bold text-indigo-900">Logo</Navbar.Brand>
-      {session === undefined && !session?.user ? (
+      {!session && !session?.user ? (
           <div className="flex md:order-2">
             <Button onClick={() => signIn('google' , { redirectTo: '/' })} className="border-2 bg-transparent border-blue-500 text-blue-500 px-2 py-2 sm:px-2 sm:py-2 rounded-full text-sm sm:text-xl font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300 shadow-lg hover:shadow-2xl w-full sm:w-auto ">Get started</Button>
             <Navbar.Toggle />
