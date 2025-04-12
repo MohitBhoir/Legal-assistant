9638b1ff45b677b0f2d4b5a3d324535c8b25e4c8
Preet12052003 <jainpreet12052003@gamil.com>
2025-04-12 22:22:38 +0530

    fixed a minor bug

--- legal-assistant/models/user.js
+++ legal-assistant/models/user.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    username: { type: String },
+    name: { type: String },
     image: { type: String },
     createdAt: { type: Date, default: Date.now },
 });
